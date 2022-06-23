db6030c885ee88b1eb6b56756e2ff43a5cbf0a8b
Ray Yoon <58504327+c4stella@users.noreply.github.com>
2022-06-22 19:00:46 -0700

    Add files via upload

---
+++ ev_db_schema.sql
@@ -0,0 +1,36 @@
+-- Create tables for data to be loaded into
+CREATE TABLE ca (
+Model TEXT,
+count INT,
+State TEXT
+);
+
+CREATE TABLE co (
+Model TEXT,
+count INT,
+State TEXT
+);
+
+CREATE TABLE tx (
+Model TEXT,
+count INT,
+State TEXT
+);
+
+CREATE TABLE nj (
+Model TEXT,
+count INT,
+State TEXT
+);
+
+CREATE TABLE tn (
+Model TEXT,
+count INT,
+State TEXT
+);
+
+CREATE TABLE va (
+Model TEXT,
+count INT,
+State TEXT
+);
